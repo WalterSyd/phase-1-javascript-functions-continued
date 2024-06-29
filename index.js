eee77820c571f5851713f24ca8df78f201ac7bf2
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-06-29 12:12:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate'){
+return `This Saturday, I want to ${defaultActivity}!`;
+}
+
+function mondayWork(activity1 = 'go to the office'){
+    return `This Monday, I will ${activity1}.`;
+}
+
+function wrapAdjective(wrapper = '*'){
+return function(adjective = "a hard worker"){
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+};
+}
+function wrapAdjective(wrapper = "||"){
+    return function(adjective = "a dedicated programmer"){
+    return  `You are ${wrapper}${adjective}${wrapper}!`; 
+    };
+}
